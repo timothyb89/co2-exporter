4b252b1cc35359adec5d8d076761a7d32031e464
Tim Buckley <timothyb89@gmail.com>
2020-07-07 18:50:57 -0600

    logger, simple-prometheus-exporter, proper cli interface

--- README.md
+++ README.md
@@ -1,14 +1,77 @@
-co2
-===
+co2-exporter
+============
 
 A JSON and Prometheus interface for CO2Meter sensors, based on
-[co2mon][https://github.com/vfilimonov/co2mon]. Targets a Rasberry Pi Zero W.
+[co2mon](https://github.com/lnicola/co2mon). Targets a Rasberry Pi Zero W.
 
 
-### raspberry pi builds
+## Usage
 
-Two Dockerfiles can be 
+ 1. Add an appropriate udev rule to e.g. `/etc/udev/rules.d/60-co2mon.rules`:
 
+    ```
+    ACTION=="add|change", SUBSYSTEMS=="usb", ATTRS{idVendor}=="04d9", ATTRS{idProduct}=="a052", MODE:="0666"
+    ```
+    (per [co2mon](https://github.com/lnicola/co2mon#permissions)'s documentation)
+ 2. Reload udev rules:
+
+    ```bash
+    udevadm control --reload
+    udevadm trigger
+    ```
+ 3. Connect a compatible sensor via USB
+ 4. Run `co2-exporter`
+
+## Notes
+
+ * Output units are temperature and CO2 PPM. The sensor outputs temperature in
+   Celsius; the metric output includes a converted Fahrenheit value.
+ * The sensor may occasionally time out
+ *
+
+## Sensor calibration notes
+
+Sensor accuracy can be hit or miss, especially between multiple sensors. These
+tips helped to get 2 sensors to agree within 50ppm:
+
+ * The sensor may take several minutes to normalize after first power-on
+ * Be sure to set the altitude ("ALti" mode) to your approximate elevation in
+   meters
+ * Manually running the calibration ("8bc" mode) outdoors or in front of an open
+   window can help
+
+## Raspberry Pi builds
+
+Dockerfiles are provided that can generate Pi compatible builds:
+ * [`Dockerfile.gnueabi`](./Dockerfile.gnueabi) for the Pi Zero (W) and
+   first-gen Pi
+ * [`Dockerfile.gnueabihf`](./Dockerfile.gnueabihf) for later Pi versions with
+   hardware floating point support
+
+To use, run:
+
+```bash
+docker build . -f Dockerfile.gnueabi -t co2-exporter:build
+```
+
+Once the container builds, extract the result:
+
+```bash
+mkdir -p /tmp/co2-exporter && docker run --rm -v /tmp/co2-exporter:/tmp/co2-exporter co2-exporter:build sh -c 'cp /project/target/arm-unknown-linux-gnu*/release/co2-exporter /tmp/co2-exporter/co2-exporter'
+```
+
+Copy /tmp/co2-exporter/co2-exporter to your Pi and run it.
+
+## systemd service
+
+Use the example systemd unit file, [`co2-exporter.service`](./co2-exporter.service):
+
+```bash
+cp co2-exporter.service /etc/systemd/system/
+
+systemctl enable co2-exporter
+systemctl start co2-exporter
+```
 
 ### prometheus example
 
@@ -22,4 +85,5 @@ docker run \
     prom/prometheus
 ```
 
-(explicit `--dns` is needed to ensure the raspi's hostname can be looked up)
+(explicit `--dns` is needed to ensure the raspi's hostname can be looked up;
+substitute the value as necessary for your local dns server)
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,71 @@
+#[macro_use] extern crate log;
+
 use std::sync::{Arc, RwLock};
+use std::sync::atomic::{AtomicUsize, Ordering};
 use std::thread;
 
+use structopt::StructOpt;
 use co2mon::{Sensor, Reading};
 use warp::Filter;
 use serde_json::{self, json};
+use simple_prometheus_exporter::{Exporter, export};
 
-fn read_thread(reading_lock: Arc<RwLock<Option<Reading>>>){
-  thread::spawn(move || {
-    let sensor = Sensor::open_default().unwrap();
+#[derive(Debug, Clone, StructOpt)]
+#[structopt(name = "co2-exporter")]
+struct Options {
+  /// port for the http server
+  #[structopt(long, short, default_value = "8080", env = "CO2_PORT")]
+  port: u16,
+}
 
-    loop {
-      let r = sensor.read().unwrap();
-      eprintln!("reading: {:?}", r);
+enum MaybeReading {
+  Ok(Reading),
+  Err(co2mon::Error),
+  None
+}
 
-      let mut latest_reading = reading_lock.write().unwrap();
-      *latest_reading = Some(r);
+fn read_thread(
+  reading_lock: Arc<RwLock<MaybeReading>>,
+  error_count: Arc<AtomicUsize>
+) {
+  let sensor = match Sensor::open_default() {
+    Ok(sensor) => sensor,
+    Err(e) => {
+      error!("could not open sensor: {}", e);
+      std::process::exit(1);
     }
+  };
+
+  thread::spawn(move || {
+    loop {
+      let reading = match sensor.read() {
+        Ok(reading) => {
+          debug!("reading: {:?}", reading);
+
+          MaybeReading::Ok(reading)
+        },
+        Err(e) => {
+          error!("error fetching sensor reading: {}", e);
+
+          error_count.fetch_and(1, Ordering::Relaxed);
+
+          MaybeReading::Err(e)
+        }
+      };
+
+      let mut latest_reading = match reading_lock.write() {
+        Ok(r) => r,
+        Err(e) => {
+          error!("error acquiring lock: {}", e);
+          break;
+        }
+      };
+
+      *latest_reading = reading;
+    }
+
+    error!("sensor thread exited unexpectedly; refer to the log for details");
+    std::process::exit(1);
   });
 }
 
@@ -23,37 +73,65 @@ fn c_to_f(temp: f32) -> f32 {
   temp * (9f32 / 5f32) + 32f32
 }
 
+fn export_reading(
+  exporter: &Exporter, reading: &MaybeReading, error_count: &Arc<AtomicUsize>
+) -> String {
+  let mut s = exporter.session();
+
+  match reading {
+    MaybeReading::Ok(r) => {
+      export!(s, "co2", r.co2(), unit = "ppm");
+
+      let temp = r.temperature();
+      export!(s, "co2_temperature", temp, unit = "c");
+      export!(s, "co2_temperature", c_to_f(temp), unit = "f");
+    },
+    MaybeReading::Err(e) => {
+      export!(s, "co2_error", 1);
+      export!(s, "co2_error", 1, kind = e.to_string());
+    },
+    MaybeReading::None => ()
+  };
+
+  export!(s, "co2_error_count", error_count.load(Ordering::Relaxed) as f64);
+
+  s.to_string()
+}
+
 #[tokio::main]
 async fn main() {
-  let latest_reading_lock = Arc::new(RwLock::new(None));
+  env_logger::init();
 
-  read_thread(latest_reading_lock.clone());
+  let opts = Options::from_args();
+  let port = opts.port;
+
+  let error_count = Arc::new(AtomicUsize::new(0));
+  let latest_reading_lock = Arc::new(RwLock::new(MaybeReading::None));
+
+  read_thread(latest_reading_lock.clone(), error_count.clone());
 
   let json_lock = Arc::clone(&latest_reading_lock);
   let r_json = warp::path("json").map(move || {
     match *json_lock.read().unwrap() {
-      Some(ref r) => warp::reply::json(&json!({
+      MaybeReading::Ok(ref r) => warp::reply::json(&json!({
         "temperature_c": r.temperature(),
         "temperature_f": c_to_f(r.temperature()),
         "co2": r.co2()
       })),
-      None => warp::reply::json(&json!(null))
+      MaybeReading::Err(ref e) => warp::reply::json(&json!({
+        "error": e.to_string()
+      })),
+      MaybeReading::None => warp::reply::json(&json!(null))
     }
   });
 
+  let exporter = Arc::new(Exporter::new());
   let metrics_lock = Arc::clone(&latest_reading_lock);
+  let metrics_error_count = Arc::clone(&error_count);
   let r_metrics = warp::path("metrics").map(move || {
-    match *metrics_lock.read().unwrap() {
-      Some(ref r) => format!(
-        "temperature{{unit=\"c\"}} {}\ntemperature{{unit=\"f\"}} {}\nco2{{}} {}",
-        r.temperature(),
-        c_to_f(r.temperature()),
-        r.co2()
-      ),
-      None => format!("")
-    }
+    export_reading(&exporter, &*metrics_lock.read().unwrap(), &metrics_error_count)
   });
 
   let routes = warp::get().and(r_json).or(r_metrics);
-  warp::serve(routes).run(([0, 0, 0, 0], 8080)).await;
+  warp::serve(routes).run(([0, 0, 0, 0], port)).await;
 }
